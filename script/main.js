ec74d31545bf2e2c175ea32d02a5a0009c438bde
goeun0012 <ggoniha2@gmail.com>
2024-01-19 17:51:04 +0900

    01-19 설화수 연습-1

--- script/main.js
+++ script/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+    /* 언어 영역 */
+    $(".select_language").click(function(){
+        $(this).toggleClass("show");
+    })// click
+
+    /* 메뉴 영역 */
+    $(".sub").hide();
+    $(".gnb_menu > li").hover(function(){
+        $(this).find(".sub").stop().slideDown();
+        $(".gnb_menu").stop().animate({height: "80px"}, 300);
+    }, function(){
+        $(this).find(".sub").stop().slideUp();
+        $(".gnb_menu").stop().animate({height: "50px"}, 300);
+    })
+
 })// jquery
